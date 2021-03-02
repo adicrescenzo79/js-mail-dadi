d6fe2649bbb272fea7ef835aa0de70f36ddfdd4f
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-03-02 15:24:49 +0100

    file iniziali

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+var mailAutorizzate = [
+  "mcrawfor@yahoo.com",
+  "matthijs@sbcglobal.net",
+  "dbanarse@mac.com",
+  "louise@mac.com",
+  "okroeger@sbcglobal.net",
+  "staffelb@me.com",
+  "tattooman@mac.com",
+  "dsowsy@mac.com",
+  "cremonini@icloud.com",
+  "psichel@yahoo.ca",
+  "techie@icloud.com",
+  "mbalazin@verizon.net",
+  "limone79@gmail.com"
+];
+
+console.log(mailAutorizzate);
+
+mailAutorizzate.push("limone79@hotmail.it");
+
+console.log(mailAutorizzate);
